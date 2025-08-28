302ad1302fe8333b55bdc34afaadd7c6a028d07e
bspanda100@gmail.com <bspanda100@gmail.com>
2025-08-28 17:28:07 +0000

    Date_minesion

--- models/Date_dimension.sql
+++ models/Date_dimension.sql
@@ -3,20 +3,8 @@ with CTE as(
     TO_TIMESTAMP(STARTED_AT),
     DATE(TO_TIMESTAMP(STARTED_AT)) as DATE_STARTED_AT,
     HOUR(TO_TIMESTAMP(STARTED_AT)) as HOUR_STARTED_AT,
-    CASE WHEN
-    DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('sat','sun')
-    then 'WEEKEND'
-    else 'BUSINESSDAY' 
-    END as DAY_TYPE,
-    CASE WHEN
-    MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
-    then 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
-    then 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
-    then 'SUMMER'
-    ELSE 'AUTUMN'
-    END AS STATION_OF_YEAR
+    {{get_day('STARTED_AT')}} as DAY_TYPE,
+    {{get_season('STARTED_AT')}} as STATION_OF_YEAR,
     from {{ source('demo', 'bike') }}
     where STARTED_AT<>'started_at'
 )
