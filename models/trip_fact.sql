ae2c915b2a8854c7d25d48d749b3c346f4545d62
bspanda100@gmail.com <bspanda100@gmail.com>
2025-08-28 18:20:20 +0000

    Trip_fact model created commit

---
+++ models/trip_fact.sql
@@ -0,0 +1,14 @@
+with Trips as (
+    select 
+    ride_id as RIDE_ID,
+    rideable_type as RIDEABLE_TYPE,
+    date(TO_TIMESTAMP(STARTED_AT)) as TRIP_DATE,
+    start_statio_id as START_STATION_ID,
+    END_STATION_ID,
+    member_csual as MEMBER_CASUAL,
+    TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(STARTED_AT),TO_TIMESTAMP(ENDED_AT)) as TRIP_DURATION_SEC
+    from {{ source('demo', 'bike') }}
+    where ride_id<>'ride_id'
+)
+
+Select * from Trips
