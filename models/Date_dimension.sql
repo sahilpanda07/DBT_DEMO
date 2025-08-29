4bf2e4dc6f6ed131cee05870fc0cf07f244b87ec
bspanda100@gmail.com <bspanda100@gmail.com>
2025-08-29 07:21:05 +0000

    correction of data 2018

--- models/Date_dimension.sql
+++ models/Date_dimension.sql
@@ -6,7 +6,7 @@ with CTE as(
     HOUR(TO_TIMESTAMP(STARTED_AT)) as HOUR_STARTED_AT,
     {{get_day('STARTED_AT')}} as DAY_TYPE,
     {{get_season('STARTED_AT')}} as STATION_OF_YEAR,
-    from {{ source('demo', 'bike') }}
+    from {{ ref('stg_bike') }}
     where STARTED_AT<>'started_at'
 )
 
---
+++ models/example/stg_bike.sql
@@ -0,0 +1,19 @@
+with BIKE as(
+    Select
+    RIDE_ID,
+    REPLACE(STARTED_AT,'"','') as STARTED_AT,
+    REPLACE(ENDED_AT,'"','') as ENDED_AT,
+    START_STATION_NAME,
+    START_STATIO_ID,
+    END_STATION_NAME,
+    END_STATION_ID,
+    START_LAT,
+    START_LNG,
+    END_LAT,
+    END_LNG,
+    MEMBER_CSUAL
+    FROM {{ source('demo', 'bike') }}
+    WHERE RIDE_ID<>'bikeid' and STARTED_AT<> '"starttime"' and STARTED_AT<> 'starttime'
+)
+
+Select * from BIKE
